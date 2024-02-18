f8b11d0e8a61ef2926813ec7d858fd0e95b9caca
Đinh Hoàng Việt <134517889+M-DinhHoangViet@users.noreply.github.com>
2024-02-18 12:26:47 +0700

    Fix

--- js/main.js
+++ js/main.js
@@ -28,6 +28,14 @@ darkModeToggle.checked = isDarkMode;
 // Xử lý sự kiện thay đổi chế độ tối
 darkModeToggle.addEventListener('change', toggleDarkMode);
 
+var btn = $('#back2top');
+$(window).scroll(function () {
+	if ($(window).scrollTop() > 300) {
+		btn.addClass('show');
+	} else {
+		btn.removeClass('show');
+	}
+});
 
 btn.on('click', function (e) {
 	e.preventDefault();
