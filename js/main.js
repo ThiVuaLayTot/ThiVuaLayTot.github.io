07c6479241fd6b4eb4a24e88884f1f24d05cab0b
Đinh Hoàng Việt <134517889+M-DinhHoangViet@users.noreply.github.com>
2024-04-30 21:27:08 +0700

    Add new style `backtotop`

--- js/main.js
+++ js/main.js
@@ -28,19 +28,25 @@ darkModeToggle.checked = isDarkMode;
 // Xử lý sự kiện thay đổi chế độ tối
 darkModeToggle.addEventListener('change', toggleDarkMode);
 
-var btn = ('#back2top');
-$(window).scroll(function() {
-    if ($(window).scrollTop() > 300) {
-        btn.addClass('show');
-    } else {
-        btn.removeClass('show');
-    }
-});
+// Get the button
+let mybutton = document.getElementById("myBtn");
 
-btn.on('click', function(e) {
-    e.preventDefault();
-    $('html, body').animate({ scrollTop: 0 }, '800');
-});
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
 
 // Topnav
 function myFunction() {
