167fe4d8211d54691ec7de253c19c146539d3078
Đinh Hoàng Việt <134517889+M-DinhHoangViet@users.noreply.github.com>
2024-05-01 16:24:08 +0700

    tweak somethings

--- python/update_time.py
+++ python/update_time.py
@@ -5,7 +5,7 @@ import pytz
 def time():
     tz_VI = pytz.timezone('Asia/Ho_Chi_Minh')
     datetime_VI = datetime.now(tz_VI)
-    time = f'<p align="right"><i>Lần cuối cập nhật nội dung của website: {datetime_VI.hour}:{datetime_VI.minute}:{datetime_VI.second}, ngày {datetime_VI.day}/{datetime_VI.month}/{datetime_VI.year}</i>.</p>'
+    time = f'<p><i>Lần cuối cập nhật nội dung của website: {datetime_VI.hour}:{datetime_VI.minute}:{datetime_VI.second}, ngày {datetime_VI.day}/{datetime_VI.month}/{datetime_VI.year}</i>.</p>'
     return time
 
 with open('_includes/updated.html', 'w', encoding='utf-8') as html_file:
