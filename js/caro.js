fd00d69ce938a5207ef7621d56448369c035503f
Đinh Hoàng Việt <134517889+M-DinhHoangViet@users.noreply.github.com>
2024-05-02 22:55:33 +0700

    ...

--- js/caro.js
+++ js/caro.js
@@ -1,34 +1,3 @@
-// JavaScript cho game sliding
-function sol(s) {
-    return s.pieces.find((p) => p.name === 'K').position === 13;
-}
-function win(s) {
-    setTimeout(() => {
-        wscreen = document.createElement('div');
-        wscreen.classList.add('win');
-        s.elements.main.appendChild(wscreen);
-    }, 50);
-}
-function move(s) {
-    mcnt = document.getElementById('move-count');
-    mcnt.innerHTML = s.moves;
-}
-
-function init() {
-    mcnt = document.getElementById('move-count');
-    mcnt.innerHTML = '0';
-    SlidingPuzzles(document.getElementById('game'), 'G1 K K G2/G1 K K G2/B S S R/B N L R/ P1 . . P2', {
-        solution: sol,
-        onVictory: win,
-        onMove: move,
-    });
-}
-
-btn = document.getElementById('reset');
-btn.onclick = init;
-init();
-
-// JavaScript cho game Tic-Tac-Toe
 function getBestMove(board, player) {
     let bestScore = -Infinity;
     let bestMove;
