b616db321f58622562faa620f8365736819effc1
Đinh Hoàng Việt <134517889+M-DinhHoangViet@users.noreply.github.com>
2024-08-11 10:13:29 +0700

    Update

--- js/main.js
+++ js/main.js
@@ -57,3 +57,53 @@ function myFunction() {
         x.className = "page-header";
     }
 }
+
+const updateButton = document.getElementById("buttonDetails");
+const cancelButton = document.getElementById("cancel");
+const dialog = document.getElementById("typeDialog");
+dialog.returnValue = "typeDialog";
+
+function openCheck(dialog) {
+    if (dialog.open) {
+        console.log("Dialog open");
+    } else {
+        console.log("Dialog closed");
+    }
+}
+
+// Update button opens a modeless dialog
+updateButton.addEventListener("click", () => {
+    dialog.show();
+    openCheck(dialog);
+});
+
+// Form cancel button closes the dialog box
+cancelButton.addEventListener("click", () => {
+    dialog.close("typeNotChosen");
+    openCheck(dialog);
+});
+
+const upButton = document.getElementById("btnDetails");
+const cancelButton = document.getElementById("cancel");
+const dialog = document.getElementById("typDialog");
+dialog.returnValue = "typDialog";
+
+function openCheck(dialog) {
+    if (dialog.open) {
+        console.log("Dialog open");
+    } else {
+        console.log("Dialog closed");
+    }
+}
+
+// Update button opens a modeless dialog
+upButton.addEventListener("click", () => {
+    dialog.show();
+    openCheck(dialog);
+});
+
+// Form cancel button closes the dialog box
+cancelButton.addEventListener("click", () => {
+    dialog.close("typeNotChosen");
+    openCheck(dialog);
+});
