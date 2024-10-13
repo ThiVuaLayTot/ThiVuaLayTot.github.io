fe0011f14c7ef544c834281b3649d32d60ed344d
Đinh Hoàng Việt <134517889+M-DinhHoangViet@users.noreply.github.com>
2024-10-13 14:30:33 +0700

    Sửa lỗi js không được dùng đúng cách

--- js/main.js
+++ js/main.js
@@ -7,7 +7,6 @@ function toggleMenu() {
     }
 }
 
-// Nút Backtotop
 let mybutton = document.getElementById("myBtn");
 
 window.onscroll = function() { scrollFunction() };
@@ -20,33 +19,7 @@ function scrollFunction() {
     }
 }
 
-// Khi click vào nút 
 function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
-
-const updateButton = document.getElementById("buttonDetails");
-const upButton = document.getElementById("btnDetails");
-const callDialog = document.getElementById("callDialog");
-const donateDialog = document.getElementById("donateDialog");
-const cancelCallButton = document.getElementById("cancelCall");
-const cancelDonateButton = document.getElementById("cancelDonate");
-
-function openCheck(dialog) {
-    console.log(dialog.open ? "Dialog open" : "Dialog closed");
-}
-
-function setupDialog(button, dialog, cancelButton) {
-    button.addEventListener("click", () => {
-        dialog.showModal();
-        openCheck(dialog);
-    });
-    cancelButton.addEventListener("click", () => {
-        dialog.close("typeNotChosen");
-        openCheck(dialog);
-    });
-}
-
-setupDialog(updateButton, callDialog, cancelCallButton);
-setupDialog(upButton, donateDialog, cancelDonateButton);
